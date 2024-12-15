fca1227aa63f8400f4217ba14d7f0452b023435d
cczzHoward <howardgg509@gmail.com>
2024-12-15 23:24:12 +0800

    1. Update some mods. 2. Encrypt the user's password before saving. 3. Add simple validation for register, login, postProduct.

--- models/users.js
+++ models/users.js
@@ -1,6 +1,7 @@
 const db = require("./db");
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const bcrypt = require("bcrypt");
 
 // 建立 users Schema
 const userSchema = new Schema(
@@ -38,6 +39,29 @@ const userSchema = new Schema(
         versionKey: false,
     }
 )
+
+// 檢查密碼是否正確
+userSchema.methods.comparePassword = async function(password) {
+    try {
+        return await bcrypt.compare(password, this.password);
+    } catch (err) {
+        throw new Error(err);
+    }
+};
+
+// 對 password 做加密處理
+userSchema.pre("save", async function(next) {
+    try {
+        if (this.isModified("password") || this.isNew) {
+            const salt = await bcrypt.genSalt(10);
+            const hash = await bcrypt.hash(this.password, salt);
+            this.password = hash;
+        }
+        next();
+    } catch (err) {
+        next(err);
+    }
+});
 
 // 建立 User Model
 const UserModel = db.model("User", userSchema);
---
+++ utils/validator.js
@@ -0,0 +1,33 @@
+const joi = require('joi');
+
+const registerValidation = (data) => {
+    const userSchema = joi.object({
+        username: joi.string().min(3).max(63).required(),
+        email: joi.string().email().required(),
+        password: joi.string().min(8).max(255).required(),
+        role: joi.string().valid('customer', 'seller', 'admin').default('customer')
+    });
+
+    return userSchema.validate(data);
+}
+
+const loginValidation = (data) => {
+    const userSchema = joi.object({
+        email: joi.string().email().required(),
+        password: joi.string().min(8).max(255).required()
+    });
+
+    return userSchema.validate(data);
+};
+
+const postProductValidation = (data) => {
+    const productSchema = joi.object({
+        title: joi.string().min(3).max(63).required(),
+        price: joi.number().min(1).max(99999).required(),
+        category: joi.string().valid('food', 'drink', 'others').required(),
+        description: joi.string().required(),
+        image: joi.string().default('').dataUri()
+    });
+
+    return productSchema.validate(data);
+}
